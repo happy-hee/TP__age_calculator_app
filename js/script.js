7d01478aa9c8a00c834d9692328e3110f5834401
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-06-20 17:22:36 +0900

    입력폼 변수명 수정

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 const submitBtn = document.querySelector("#submitBtn");
 
 submitBtn.addEventListener("click", () => {
-  let ageDay = document.querySelector("#day");
+  let inputDay = document.querySelector("#day");
   let errorDayText = document.querySelector(".error-text--day"); //day 에러 메세지
-  let ageMonth = document.querySelector("#month");
+  let inputMonth = document.querySelector("#month");
   let errorMonthText = document.querySelector(".error-text--month"); //month 에러 메세지
-  let ageYear = document.querySelector("#year");
+  let inputYear = document.querySelector("#year");
   let errorYearText = document.querySelector(".error-text--year"); //year 에러 메세지
 
   /**
    * Day 입력값 유효성 검사
    */
-  let monthLastDay = new Date(ageYear.value, ageMonth.value, 0).getDate(); // 월의 마지막 날짜
+  let monthLastDay = new Date(inputYear.value, inputMonth.value, 0).getDate(); // 월의 마지막 날짜
 
   // 값이 비어있을 경우
-  if (!ageDay.value) {
-    ageDay.parentElement.classList.add("error-form");
-    ageDay.nextElementSibling.classList.add("dp-block");
+  if (!inputDay.value) {
+    inputDay.parentElement.classList.add("error-form");
+    inputDay.nextElementSibling.classList.add("dp-block");
     errorDayText.classList.remove("dp-block");
   } else if (
     // day가 1~31 사이의 숫자가 아닌 경우
-    (ageDay.value && (1 > ageDay.value || ageDay.value > 31)) ||
+    (inputDay.value && (1 > inputDay.value || inputDay.value > 31)) ||
     // 해당 월에 해당하지 않는 day를 입력할 경우
-    monthLastDay < ageDay.value
+    monthLastDay < inputDay.value
   ) {
-    ageDay.parentElement.classList.add("error-form");
-    ageDay.nextElementSibling.classList.remove("dp-block");
+    inputDay.parentElement.classList.add("error-form");
+    inputDay.nextElementSibling.classList.remove("dp-block");
     errorDayText.classList.add("dp-block");
   } else {
-    ageDay.parentElement.classList.remove("error-form");
-    ageDay.nextElementSibling.classList.remove("dp-block");
+    inputDay.parentElement.classList.remove("error-form");
+    inputDay.nextElementSibling.classList.remove("dp-block");
     errorDayText.classList.remove("dp-block");
   }
 
@@ -37,21 +37,21 @@ submitBtn.addEventListener("click", () => {
    * Month 입력값 유효성 검사
    */
   // 값이 비어있을 경우
-  if (!ageMonth.value) {
-    ageMonth.parentElement.classList.add("error-form");
-    ageMonth.nextElementSibling.classList.add("dp-block");
+  if (!inputMonth.value) {
+    inputMonth.parentElement.classList.add("error-form");
+    inputMonth.nextElementSibling.classList.add("dp-block");
     errorMonthText.classList.remove("dp-block");
   } else if (
-    ageMonth.value &&
+    inputMonth.value &&
     // month가 1~12 사이의 숫자가 아닌 경우
-    (1 > ageMonth.value || ageMonth.value > 12)
+    (1 > inputMonth.value || inputMonth.value > 12)
   ) {
-    ageMonth.parentElement.classList.add("error-form");
-    ageMonth.nextElementSibling.classList.remove("dp-block");
+    inputMonth.parentElement.classList.add("error-form");
+    inputMonth.nextElementSibling.classList.remove("dp-block");
     errorMonthText.classList.add("dp-block");
   } else {
-    ageMonth.parentElement.classList.remove("error-form");
-    ageMonth.nextElementSibling.classList.remove("dp-block");
+    inputMonth.parentElement.classList.remove("error-form");
+    inputMonth.nextElementSibling.classList.remove("dp-block");
     errorMonthText.classList.remove("dp-block");
   }
 
@@ -60,21 +60,21 @@ submitBtn.addEventListener("click", () => {
    */
   let crrYear = new Date().getFullYear();
   // 값이 비어있을 경우
-  if (!ageYear.value) {
-    ageYear.parentElement.classList.add("error-form");
-    ageYear.nextElementSibling.classList.add("dp-block");
+  if (!inputYear.value) {
+    inputYear.parentElement.classList.add("error-form");
+    inputYear.nextElementSibling.classList.add("dp-block");
     errorYearText.classList.remove("dp-block");
   } else if (
-    ageYear.value &&
+    inputYear.value &&
     // year이 미래일 경우
-    ageYear.value > crrYear
+    inputYear.value > crrYear
   ) {
-    ageYear.parentElement.classList.add("error-form");
-    ageYear.nextElementSibling.classList.remove("dp-block");
+    inputYear.parentElement.classList.add("error-form");
+    inputYear.nextElementSibling.classList.remove("dp-block");
     errorYearText.classList.add("dp-block");
   } else {
-    ageYear.parentElement.classList.remove("error-form");
-    ageYear.nextElementSibling.classList.remove("dp-block");
+    inputYear.parentElement.classList.remove("error-form");
+    inputYear.nextElementSibling.classList.remove("dp-block");
     errorYearText.classList.remove("dp-block");
   }
 });
